739b27136c37513ff0aecdea8e683c55f6a37a18
iD Student <idstudent@iDs-iMac.local>
2017-07-19 16:55:34 -0700

    set up initial UI

--- flashcard/ViewController.swift
+++ flashcard/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionLabel: UILabel!
+    @IBOutlet weak var questonTV1: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
